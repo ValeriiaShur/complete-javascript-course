4ab196b717c5c45382d954fb80e47cf74473fbfd
ValeriiaShur <valeriia.shurupina@yahoo.com>
2021-01-06 23:49:16 +0100

    First three lectures

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,57 @@
+// LECTURE: Values and Variables
+
+/* let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Bob";
+
+
+console.log(firstName); */
+
+
+
+// LECTURE: Data Types
+
+let javascriptIsFun = true;
+//console.log(javascriptIsFun);
+
+//console.log(typeof true);
+//console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+//console.log(typeof javascriptIsFun);
+
+let year;
+//console.log(year);
+//console.log(typeof year);
+
+year = 1991;
+//console.log(typeof year);
+
+//console.log(typeof null); //retuns the type of object, which is a JS bug
+
+
+
+// LECTURE: let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schmedtmann'; //better always declare let or var
+//console.log(lastName);
+
+
+
+// LECTURE: Basic Operators
