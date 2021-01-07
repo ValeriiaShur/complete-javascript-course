e0f717d4c6b389d003781fdf29e321510a759bcd
ValeriiaShur <valeriia.shurupina@yahoo.com>
2021-01-07 23:06:20 +0100

    Lecture on basic operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -4,8 +4,9 @@
 
 // LECTURE: Values and Variables
 
-/* 1.Declare variables called 'country', 'continent' and 'population' and
-assign their values according to your own country (population in millions)*/
+/* 1.Declare variables called 'country', 'continent' and 
+'population' and assign their values according to your own 
+country (population in millions) */
 const country = 'Russia';
 const continent = 'Europe';
 let population = 144500000;
@@ -27,14 +28,15 @@ let population = 144500000;
 7. BigInt (ES2020)
 */
 
-/* 1. Declare a variable called 'isIsland' and set its value according to your
-country.The variable should hold a Boolean value.Also declare a variable
-'language', but don't assign it any value yet */
+/* 1. Declare a variable called 'isIsland' and set its value 
+according to your country.The variable should hold a Boolean 
+value.Also declare a variable 'language', but don't assign it 
+any value yet */
 const isIsland = false;
 let language;
 
-/* 2. Log the types of 'isIsland', 'population', 'country' and 'language'
-to the console */
+/* 2. Log the types of 'isIsland', 'population', 'country' and 
+'language' to the console */
 //console.log(typeof isIsland);
 //console.log(typeof population);
 //console.log(typeof country);
@@ -44,37 +46,55 @@ to the console */
 
 // LECTURE: let, const and var
 
-/* 1. Set the value of 'language' to the language spoken where you live (some
-countries have multiple languages, but just choose one) */
+/* 1. Set the value of 'language' to the language spoken where 
+you live (some countries have multiple languages, but just 
+choose one) */
 language = 'Dutch';
 
 
-/* 2. Think about which variables should be const variables (which values will never
-change, and which might change?). Then, change these variables to const. */
+/* 2. Think about which variables should be const variables
+(which values will never change, and which might change?).
+Then, change these variables to const. */
 
 
-/* 3. Try to change one of the changed variables now, and observe what happens */
+/* 3. Try to change one of the changed variables now, and
+observe what happens */
 // isIsland = true; // returns Uncaught TypeError: Assignment to constant variable
 
 
 
 // LECTURE: Basic Operators
-/* 1. If your country split in half, and each half would contain half the population,
-then how many people would live in each half? */
+/* 1. If your country split in half, and each half would
+contain half the population, then how many people would live
+in each half? */
+let halfPopulation = population / 2;
+//console.log(halfPopulation)
+
+/* 2. Increase the population of your country by 1 and log
+the result to the console */
+population++;
+//console.log(population)
+
+/* 3. Finland has a population of 6 million. Does your country
+have more people than Finland? */
+finlandPopulation = 6000000;
+//console.log(population > finlandPopulation)
+
+/* 4. The average population of a country is 33 million people.
+Does your country have less people than the average country? */
+averagePopulation = 33000000;
+//console.log(population > averagePopulation);
+
+/* 5. Based on the variables you created, create a new variable
+'description' which contains a string with this format:
+'Portugal is in Europe, and its 11 million people speak
+portuguese' */
+description = country + ' is in ' + continent + ', and its ' + ((population - 1) / 1000000) + ' million people speak Russian';
+//console.log(description)
 
 
-/* 2. Increase the population of your country by 1 and log the result to the console */
 
+// LECTURE: Strings and Template Literals
 
-/* 3. Finland has a population of 6 million. Does your country have more people than
-Finland? */
-
-
-/* 4. The average population of a country is 33 million people. Does your country
-have less people than the average country? */
-
-
-/* 5. Based on the variables you created, create a new variable 'description'
-which contains a string with this format: 'Portugal is in Europe, and its 11 million
-people speak portuguese' */
-
+/* 1. Recreate the 'description' variable from the last
+assignment, this time using the template literal syntax */
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -33,7 +33,8 @@ let year;
 year = 1991;
 //console.log(typeof year);
 
-//console.log(typeof null); //retuns the type of object, which is a JS bug
+/*console.log(typeof null); retuns the type of object,
+which is a JS bug * /
 
 
 
@@ -55,3 +56,46 @@ lastName = 'Schmedtmann'; //better always declare let or var
 
 
 // LECTURE: Basic Operators
+
+/* Operator helps to transform values or combine multiple
+values
+
+Categories:
+1. Mathematical
+2. Comparison
+3. Logical
+4. Assignment
+5. etc. */
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+//console.log(ageJonas, ageSarah);
+
+//console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedmann';
+//console.log(firstName + ' ' + lastName);
+
+//Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+//console.log(x);
+
+// Comparison operators
+//console.log(ageJonas > ageSarah); // >, <, >=, <=
+//console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018);
+
+
+
+// LECTURE: Strings and Template Literals
