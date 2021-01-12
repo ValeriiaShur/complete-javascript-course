bbe1f3d5f4646c61c7dc05190cd0cd51165725ed
ValeriiaShur <valeriia.shurupina@yahoo.com>
2021-01-12 14:01:45 +0100

    +2 lectures, coding challenges done

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -90,7 +90,7 @@ averagePopulation = 33000000;
 'Portugal is in Europe, and its 11 million people speak
 portuguese' */
 description = country + ' is in ' + continent + ', and its ' + ((population - 1) / 1000000) + ' million people speak Russian';
-//console.log(description)
+// console.log(description)
 
 
 
@@ -98,3 +98,26 @@ description = country + ' is in ' + continent + ', and its ' + ((population - 1)
 
 /* 1. Recreate the 'description' variable from the last
 assignment, this time using the template literal syntax */
+descriptionNew = `${country} is in ${continent}, and its ${((population - 1) / 1000000)} million people speak Russian`;
+// console.log(descriptionNew)
+
+
+
+// LECTURE: Taking Decisions: if / else Statements
+
+/* 1. If your country's population is greater that 33 million,
+log a string like this to the console: 'Portugal's population
+is above average'. Otherwise, log a string like 'Portugal's
+population is 22 million below average' (the 22 is the average
+of 33 minus the country's population) */
+if (population > 33000000) {
+    // console.log(`${country}'s population is above average`);
+} else {
+    // console.log(`${country}'s population is below average`);
+}
+
+/* 2. After checking the result, change the population temporarily
+to 13 and then to 130. See the different results, and set the
+population back to original */
+
+
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -26,11 +26,11 @@ let javascriptIsFun = true;
 javascriptIsFun = 'YES!';
 //console.log(typeof javascriptIsFun);
 
-let year;
+// let year;
 //console.log(year);
 //console.log(typeof year);
 
-year = 1991;
+// year = 1991;
 //console.log(typeof year);
 
 /*console.log(typeof null); retuns the type of object,
@@ -99,3 +99,98 @@ const isFullAge = ageSarah >= 18;
 
 
 // LECTURE: Strings and Template Literals
+let a, b;
+a = b = 25 - 10 - 5;
+//console.log(a, b);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah, averageAge);
+
+
+
+//////////////////////////
+//  Coding Challenge #1 //
+//////////////////////////
+
+// 1. Store Mark's and John's mass and height in variables
+massMark = 78;
+massJohn = 92;
+heightMark = 1.69;
+heightJohn = 1.95;
+
+// 2. Calculate both their BMIs using the formula (you can 
+// even implement both versions)
+BMIMark = massMark / heightMark ** 2;
+BMIJohn = massJohn / heightJohn ** 2;
+
+// 3. Create a Boolean variable 'markHigherBMI' containing 
+// information about whether Mark has a higher BMI than John.
+markHigherBMI = BMIMark > BMIJohn;
+
+// console.log(markHigherBMI);
+
+
+
+// LECTURE: Strings and Template Literals
+const job = 'teacher';
+const birthYear = 1991;
+year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+// console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+// console.log(jonasNew);
+// console.log(`Just a regular string...`);
+/* console.log(`String with \n\
+multiple \n\
+lines`); */
+
+
+
+// LECTURE: Taking Decisions: if / else Statements
+const age = 15;
+
+if (age >= 18) {
+    // console.log(`Sarah can start driving license!`);
+} else {
+    const yearsLeft = 18 - age;
+    // console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`)
+}
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+// console.log(century);
+
+
+
+//////////////////////////
+//  Coding Challenge #2 //
+//////////////////////////
+
+/* 1. Print a nice output to the console, saying who has the
+higher BMI. The message is either "Mark's BMI is higher than
+John's!" or "John's BMI is higher than Mark's!" */
+
+/* if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI is higher than John's!`);
+} else {
+    console.log(`John's BMI is higher than Mark's!`);
+} */
+
+
+/* 2. Use a template literal to include the BMI values in the
+outputs. Example: "Mark's BMI (28.3) is higher than John's
+(23.9)!" */
+
+/* if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+} */
+
