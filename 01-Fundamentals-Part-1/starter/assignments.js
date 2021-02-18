c06344e353bc3f1f2ef046aee57ac1d27d33fc07
ValeriiaShur <valeriia.shurupina@yahoo.com>
2021-02-18 14:53:19 +0100

    Completed

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -33,7 +33,7 @@ according to your country.The variable should hold a Boolean
 value.Also declare a variable 'language', but don't assign it 
 any value yet */
 const isIsland = false;
-let language;
+// let language;
 
 /* 2. Log the types of 'isIsland', 'population', 'country' and 
 'language' to the console */
@@ -49,7 +49,7 @@ let language;
 /* 1. Set the value of 'language' to the language spoken where 
 you live (some countries have multiple languages, but just 
 choose one) */
-language = 'Dutch';
+// language = 'Dutch';
 
 
 /* 2. Think about which variables should be const variables
@@ -120,4 +120,153 @@ if (population > 33000000) {
 to 13 and then to 130. See the different results, and set the
 population back to original */
 
+
+
+// LECTURE: Type Conversion and Coercion
+/* 1. Predict the result of these 5 operations without
+executing them:
+'9' - '5';  = 4
+'19' - '13' + '17'; = 617
+'19' - '13' + 17; = 23
+'123' < 57; = false
+5 + 6 + '4' + 9 - 4 - 2; = 1143 */
+
+/* 2. Execute the operations to check if you were right */
+/* console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2); */
+
+
+
+// LECTURE: Equality Operators: == vs. ===
+
+/* 1. Declare a variable 'numNeighbours' based on a
+prompt input like this: prompt('How many neighbour
+countries does your country have?'); */
+// const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+
+/* 2. If there is only 1 neighbour, log to the console
+'Only 1 border!' (use loose equality == for now) */
+// if (numNeighbours == 1) console.log(`Only 1 border!`)
+
+/* 3. Use an else-if block to log 'More than 1 border'
+in case 'numNeighbours' is greater than 1 */
+/* if (numNeighbours == 1) {
+    console.log(`Only 1 border!`);
+} else if (numNeighbours > 1) {
+    console.log(`More than 1 border!`);
+} */
+
+
+/* 4. Use an else block to log 'No borders' (this block
+will be executed when 'numNeighbours' is 0 or any other
+value) */
+/* if (numNeighbours == 1) {
+    console.log(`Only 1 border!`);
+} else if (numNeighbours > 1) {
+    console.log(`More than 1 border!`);
+} else if (numNeighbours < 1) {
+    console.log(`No borders!`);
+} */
+
+/* 5. Test the code with different values of
+'numNeighbours', including 1 and 0. */
+
+
+/* 6. Change == to ===, and test the code again, with
+the same values of 'numNeighbours'. Notice what happens
+when there is exactly 1 border! Why is this happening? */
+/* if (numNeighbours === 1) {
+    console.log(`Only 1 border!`);
+} else if (numNeighbours > 1) {
+    console.log(`More than 1 border!`);
+} else if (numNeighbours < 1) {
+    console.log(`No borders!`);
+} */
+
+/* 7. Finally, convert 'numNeighbours' to a number, and
+watch what happens now when you input 1 */
+/* 8. Reflect on why we should use the === operator and
+type conversion in this situation */
+
+
+
+// LECTURE: Logical Operators
+/* 1. Comment out the previous code so the prompt
+doesn't get in the way */
+/* 2. Let's say Sarah is looking for a new country to
+live in. She wants to live in a country that speaks
+english, has less than 50 million people and is not an
+island. */
+/* 3. Write an if statement to help Sarah figure out if
+your country is right for her. You will need to write a
+condition that accounts for all of Sarah's criteria.
+Take your time with this, and check part of the
+solution if necessary. */
+/* if (language === 'English' && population < 50000000 && continent != 'island') {
+    console.log('You should live in Portugal :)');
+} else {
+    console.log('Portugal does not meet your criteria :(');
+} */
+
+/* 4. If yours is the right country, log a string like
+this: 'You should live in Portugal :)'. If not, log
+'Portugal does not meet your criteria :(' */
+/* 5. Probably your country does not meet all the
+criteria. So go back and temporarily change some
+variables in order to make the condition true (unless
+you live in Canada :D) */
+
+
+
+// LECTURE: The switch Statement
+/* 1. Use a switch statement to log the following string for
+the given 'language':
+chinese or mandarin: 'MOST number of native speakers!'
+spanish: '2nd place in number of native speakers'
+english: '3rd place'
+hindi: 'Number 4'
+arabic: '5th most spoken language'
+for all other simply log 'Great language too :D' */
+
+/* const language = 'mandarin';
+
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log(`MOST number of native speakers!`);
+        break;
+    case 'spanish':
+        console.log(`2nd place in number of native speakers`);
+        break;
+    case 'english':
+        console.log(`3rd place`);
+        break;
+    case 'hindi':
+        console.log(`Number 4`);
+        break;
+    case 'arabic':
+        console.log(`5th most spoken language`);
+        break;
+    default:
+        console.log(`Great language too :D`);
+} */
+
+
+
+// LECTURE: The Conditional (Ternary) Operator
+/* 1. If your country's population is greater than 33 million,
+use the ternary operator to log a string like this to the
+console: 'Portugal's population is above average'. Otherwise,
+simply log 'Portugal's population is below average'. Notice
+how only one word changes between these two sentences! */
+/* const populationIsGreater = population > 33000000 ? `${country}'s population is above average` :
+    `${country}'s population is below average`;
+console.log(populationIsGreater); */
+
+/* 2. After checking the result, change the population
+temporarily to 13 and then to 130. See the different results,
+and set the population back to original */
 
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -149,7 +149,7 @@ lines`); */
 
 
 // LECTURE: Taking Decisions: if / else Statements
-const age = 15;
+/* const age = 15;
 
 if (age >= 18) {
     // console.log(`Sarah can start driving license!`);
@@ -163,7 +163,7 @@ if (birthYear <= 2000) {
     century = 20;
 } else {
     century = 21;
-}
+} */
 
 // console.log(century);
 
@@ -194,3 +194,259 @@ outputs. Example: "Mark's BMI (28.3) is higher than John's
     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
 } */
 
+
+
+// LECTURE: Type Conversion and Coercion
+
+// type conversion - manual explicit conversion
+const inputYear = '1991';
+const inputYearConverted = Number(inputYear);
+//console.log(inputYearConverted + 18);
+
+//console.log(String(23));
+
+// type coercion - convert values of one type to another
+/* console.log(`I am ` + 23 + ` years old`);
+console.log(`I am ` + `23` + ` years old`);
+console.log('23' - '10' - 3); // numbers
+console.log('23' + '10' + 3); // strings
+console.log('23' * '10'); // numbers
+console.log('23' > '10'); // numbers */
+
+/* let n = '1' + 1;
+n = n - 1;
+console.log(n); // result is a number 10 */
+
+
+
+// LECTURE: Truthy and Falsy Values
+
+/* 5 falsy values:
+1. 0
+2. ''
+3. undefined
+4. null
+5. NaN
+ */
+/* console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(''));
+console.log(Boolean({}));
+
+const money = 0;
+if (money) {
+    console.log(`Don't spend it all ;)`);
+} else {
+    console.log(`You should get a job!`);
+}
+
+let height;
+if (height) {
+    console.log(`YAY! Height is defined`);
+} else {
+    console.log(`Height is UNDEFINED`)
+} */
+
+
+
+// LECTURE: Equality Operators: == vs. ===
+/* const age = '18';
+if (age === 18) console.log(`You just became an adult! (strict)`)
+
+if (age == 18) console.log(`You just became an adult! (loose)`)
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(typeof favourite);
+
+if (favourite === 23) {
+    console.log(`Cool! ${favourite} is an amazing number!`);
+} else if (favourite === 7) {
+    console.log(`${favourite} is also an cool number!`);
+} else {
+    console.log(`Number is not 23 or 7`);
+}
+
+// 'different' or not equal operator
+if (favourite !== 23) console.log(`Why not 23?`); */
+
+
+
+// LECTURE: Logical Operators
+const hasDriverLisence = true; // A
+const hasGoodVision = true; // B
+
+/* console.log(hasDriverLisence && hasGoodVision); // AND
+console.log(hasDriverLisence || hasGoodVision); // OR
+console.log(!hasDriverLisence); // NOT */
+
+const shouldDrive = hasDriverLisence && hasGoodVision;
+/* if (shouldDrive) {
+    console.log(`Sarah is able to drive!`);
+} else {
+    console.log(`Someone else should drive...`);
+} */
+
+/* const isTired = false; // C
+console.log(hasDriverLisence || hasGoodVision || isTired);
+
+if (hasDriverLisence && hasGoodVision && !isTired) {
+    console.log(`Sarah is able to drive!`);
+} else {
+    console.log(`Someone else should drive...`);
+} */
+
+
+
+//////////////////////////
+//  Coding Challenge #3 //
+//////////////////////////
+/* There are two gymnastics teams, Dolphins and Koalas. 
+They compete against each other 3 times. The winner 
+with the highest average score wins a trophy! Your tasks:
+1. Calculate the average score for each team, using the 
+test data below */
+dolphinesScore1 = 96;
+dolphinesScore2 = 108;
+dolphinesScore3 = 89;
+koalasScore1 = 88;
+koalasScore2 = 91;
+koalasScore3 = 110;
+dolphinesAverageScore = (dolphinesScore1 + dolphinesScore2 + dolphinesScore3) / 3;
+koalasAverageScore = (koalasScore1 + koalasScore2 + koalasScore3) / 3;
+
+/* 2. Compare the team's average scores to determine
+the winner of the competition, and print it to the
+console. Don't forget that there can be a draw, so test
+for that as well (draw means they have the same average
+score) */
+/* if (dolphinesAverageScore > koalasAverageScore) {
+    console.log(`Dolphines are the winners (${dolphinesAverageScore})!`);
+} else if (dolphinesAverageScore < koalasAverageScore) {
+    console.log(`Koalas are the winners (${koalasAverageScore})!`);
+} else if (dolphinesAverageScore === koalasAverageScore) {
+    console.log(`It is a draw (${koalasAverageScore})!`);
+} */
+
+
+/* 3. Bonus 1: Include a requirement for a minimum
+score of 100. With this rule, a team only wins if it
+has a higher score than the other team, and the same
+time a score of at least 100 points. Hint: Use a
+logical operator to test for minimum score, as well as
+multiple else-if blocks � */
+/* if (dolphinesAverageScore > koalasAverageScore && dolphinesAverageScore > 100) {
+    console.log(`Dolphines are the winners (${dolphinesAverageScore})!`);
+} else if (dolphinesAverageScore < koalasAverageScore && koalasAverageScore > 100) {
+    console.log(`Koalas are the winners (${koalasAverageScore})!`);
+} else if (dolphinesAverageScore === koalasAverageScore) {
+    console.log(`It is a draw (${koalasAverageScore})!`);
+} else console.log(`No winners`); */
+
+
+/* 4. Bonus 2: Minimum score also applies to a draw! So
+a draw only happens when both teams have the same score
+and both have a score greater or equal 100 points.
+Otherwise, no team wins the trophy */
+/* if (dolphinesAverageScore > koalasAverageScore && dolphinesAverageScore > 100) {
+    console.log(`Dolphines are the winners (${dolphinesAverageScore})!`);
+} else if (dolphinesAverageScore < koalasAverageScore && koalasAverageScore > 100) {
+    console.log(`Koalas are the winners (${koalasAverageScore})!`);
+} else if ((dolphinesAverageScore === koalasAverageScore) && dolphinesAverageScore > 100 && koalasAverageScore > 100) {
+    console.log(`It is a draw (${koalasAverageScore})!`);
+} else console.log(`No winners`); */
+
+
+
+// LECTURE: The switch Statement
+const day = 'monday';
+
+/* switch (day) {
+    case 'monday':
+        console.log(`Plan course structure`);
+        console.log(`Go to coding meetup`);
+        break;
+    case 'tuesday':
+        console.log(`Prepare theory vedeos`);
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log(`Write code examples`);
+        break;
+    case 'friday':
+        console.log(`Record videos`);
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log(`Enjoy the weekend!`);
+        break;
+    default:
+        console.log(`Not a valid day!`)
+}
+
+// IF statement
+if (day === 'monday') {
+    console.log(`Plan course structure`);
+    console.log(`Go to coding meetup`);
+} else if (day === 'tuesday') {
+    console.log(`Prepare theory vedeos`);
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log(`Write code examples`);
+} else if (day === 'friday') {
+    console.log(`Record videos`);
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log(`Enjoy the weekend!`);
+} else {
+    console.log(`Not a valid day!`);
+} */
+
+
+
+// LECTURE: The Conditional (Ternary) Operator
+
+// Expression is a piece of code that produces value
+// Example: 3 + 4
+// Example: 1991
+// Example: true && false && !falsee
+
+// Statement is a sequence of actions
+/* if (23 > 10) {
+    const str = '23 is bigger';
+}
+
+const age = 23;
+age >= 18 ? console.log(`I like to drink wine`) :
+    console.log(`I like to drink water`);
+
+const drink = age >= 18 ? `wine` : `water`;
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? `wine` : `water`}`); */
+
+
+
+//////////////////////////
+//  Coding Challenge #4 //
+//////////////////////////
+
+// Switch operator
+const bill = 100;
+switch (bill) {
+    case bill >= 50 && bill <= 300:
+        console.log(`The bill was ${bill}, the tip was ${tip = bill * 15 / 100}, and the total value ${bill + tip}`);
+        break;
+    default:
+        console.log(`The bill was ${bill}, the tip was ${tip = bill * 20 / 100}, and the total value ${bill + tip}`);
+}
+
+// Ternary operator
+const calc = bill >= 50 && bill <= 300 ? `The bill was ${bill}, the tip was ${tip = bill * 15 / 100}, and the total value ${bill + tip}` :
+    `The bill was ${bill}, the tip was ${tip = bill * 20 / 100}, and the total value ${bill + tip}`;
+console.log(calc);
