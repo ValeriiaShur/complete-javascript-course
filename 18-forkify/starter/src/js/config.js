d0edbe597e230371cf21d766ddacda4bdedb2fb6
ValeriiaShur <valeriia.shurupina@yahoo.com>
2021-04-08 14:41:56 +0200

    Constants of the project

---
+++ 18-forkify/starter/src/js/config.js
@@ -0,0 +1,6 @@
+// All constants that gonna be reused
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `4937b1da-f497-4dfb-abe0-44a1eae48ca2`;
+export const MODAL_CLOSE_SEC = 2.5;
